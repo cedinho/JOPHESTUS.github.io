3b45ab6948ce233eff7b5f4607ca638ba2443da0
JOPHESTUS <JOPHESTUS@JOPHEST.US>
2013-06-01 15:56:37 +1000

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!-- Design by Free CSS Templates http://www.freecsstemplates.org Released for free under a Creative Commons Attribution 2.5 License Name : GradientBlack Description: A two-column, fixed-width design with dark color scheme. Version : 1.0 Released : 20111121 -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Black Minor</title>
+  <link href="http://fonts.googleapis.com/css?family=Arvo"
+ rel="stylesheet" type="text/css" />
+  <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+    <style type="text/css">
+        .style1
+        {
+            color: #000000;
+        }
+    </style>
+</head>
+<body>
+<div id="wrapper">
+<div class="aligncenter" id="header-wrapper">
+<div id="header">
+<div id="logo">&nbsp;
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp; <img style="width: 520px; height: 92px;" alt=""
+ src="images/logo.png" /></div>
+</div>
+</div>
+<!-- end #header -->
+<div style="font-family: Venus Rising;" id="menu">
+<ul>
+  <li class="current_page_item aligncenter"><a href="http://blackminor.co.cc">&nbsp;<big><big><span
+ style="font-family: Venus Rising;">HOME</span></big></big></a></li>
+  <li><big><big><a href="http://forums.blackminor.co.cc"><big><big><span
+ style="font-family: Venus Rising;">FORUMS</span></big></big></a></big></big></li>
+  <li><big><big><a href="http://blackminor.co.cc/rules.html"><big><big><span
+ style="font-family: Venus Rising;">RULES</span></big></big></a></big></big></li>
+  <li><big><big><a href="http://blackminor.co.cc/ranks.html"><big><big><span
+ style="font-family: Venus Rising;">RANKS</span></big></big></a></big></big></li>
+
+ <li><big><big><a href="http://blackminor.co.cc/wiki/"><big><big><span
+style="font-family: Venus Rising;">WIKI</span></big></big></a></big></big></li>
+  <li></li>
+</ul>
+</div>
+<!-- end #menu -->
+<div id="page">
+<div id="page-bgtop">
+<div id="page-bgbtm">
+<div id="content">
+<div class="post">
+<h1 class="style1">
+    Hi, Your IP address has just been logged.</h1>
+    <h1>
+
+    <img alt="" src="http://blackminor.co.cc/jophisgay/troll.png" /></h1>
+    <p>&nbsp;</p>
+<div class="entry">
+<?php
+     $logfile= ‘ip.html’;
+     $IP = $_SERVER['REMOTE_ADDR'];
+     $logdetails=  date(“F j, Y, g:i a O T”) . ‘: ‘ . ‘<a href=http://www.geobytes.com/IpLocator.htm?GetLocation&ipaddress=’.$_SERVER['REMOTE_ADDR'].’>’ .$_SERVER['REMOTE_ADDR'].’</a>’;
+     $fp = fopen($logfile, “a”);  fwrite($fp, $logdetails);  fwrite($fp, “<br>”);
+ fclose($fp);
+ echo(“Your IP has been logged. It is $IP”);
+?>
+</div>
+</div>
+
+<div style="clear: both;"><font ="Venus Rising">&nbsp;</font></div>
+</div>
+<!-- end #content --><br />
+<br />
+<br />
+<br />
+<br />
+<br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+<br />
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+<br />
+<br />
+
+</div>
+</div>
+<!-- end #page --></div>
+<div id="footer">
+<p><font ="Venus Rising">Copyright &copy; 2012
+Black Minor. All rights reserved.&nbsp;</font></p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
